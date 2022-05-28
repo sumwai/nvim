549923c2be8a4e95bbbcb5ca4c1c7fe4553a49c6
sumwai <sumwai@qq.com>
2022-05-28 17:24:39 +0800

    feat(keymap): add keymaps manager, and use short key `b*` to manager buffers.

--- init.lua
+++ init.lua
@@ -3,3 +3,7 @@ require('package-manager')
 
 -- theme and layout
 require('theme')
+
+-- keymaps
+
+require('keymaps')
---
+++ lua/keymaps.lua
@@ -0,0 +1,7 @@
+------------------------
+--- buffer line maps ---
+------------------------
+vim.cmd "nmap bq <cmd>bdelete<cr>"
+vim.cmd "nmap bp <cmd>bprevious<cr>"
+vim.cmd "nmap bn <cmd>bnext<cr>"
+
