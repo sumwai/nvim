a345fa09fb203f30a4010a58250dbf55f3e5d2fd
sumwai <sumwai@qq.com>
2022-05-28 17:42:28 +0800

    feat(keymap): use `Ctrl`+`z` to ToggleTerm.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,11 +1,24 @@
 ------------------------
 --- buffer line maps ---
 ------------------------
+
 vim.cmd "nmap bq <cmd>bdelete<cr>"
 vim.cmd "nmap bp <cmd>bprevious<cr>"
 vim.cmd "nmap bn <cmd>bnext<cr>"
+
 ------------------------
 ---- nvim tree maps ----
 ------------------------
+
 vim.cmd "nmap ` <cmd>NvimTreeToggle<cr>"
+
+------------------------
+--- toggle term maps ---
+------------------------
+
+vim.cmd "nmap <c-z> <cmd>ToggleTerm<cr>"
+
+vim.cmd "autocmd TermEnter term://*toggleterm#* tnoremap <C-z> <Cmd>exe v:count1 . 'ToggleTerm'<CR>"
+
+
 
