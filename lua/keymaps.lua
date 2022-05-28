239722d7f678f54e6e7869381a97dd3bc535f7ee
sumwai <sumwai@qq.com>
2022-05-28 17:28:30 +0800

    feat(keymap): use shortcut key '`' to toggle nvim tree.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,4 +4,8 @@
 vim.cmd "nmap bq <cmd>bdelete<cr>"
 vim.cmd "nmap bp <cmd>bprevious<cr>"
 vim.cmd "nmap bn <cmd>bnext<cr>"
+------------------------
+---- nvim tree maps ----
+------------------------
+vim.cmd "nmap ` <cmd>NvimTreeToggle<cr>"
 
