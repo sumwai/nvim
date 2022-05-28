1c9be8df38ab6e3524da97583f24574a1404503a
sumwai <sumwai@qq.com>
2022-05-28 18:30:30 +0800

    fix: when close buffer, offset will invaild. it is not the best method to fix it, but now... fix it.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,7 +2,7 @@
 --- buffer line maps ---
 ------------------------
 
-vim.cmd "nmap bq <cmd>bdelete<cr>"
+vim.cmd "nmap bq <cmd>bdelete<cr><cmd>NvimTreeToggle<cr><cmd>NvimTreeToggle<cr><cmd>wincmd p<cr>"
 vim.cmd "nmap bp <cmd>bprevious<cr>"
 vim.cmd "nmap bn <cmd>bnext<cr>"
 
